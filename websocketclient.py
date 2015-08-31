ffcc54ac943cd238d8880fec882d4ed487afe6e2
Your Name <you@example.com>
2015-08-31 14:45:30 +0200

    Added websokets

---
+++ websocketclient.py
@@ -0,0 +1,36 @@
+import websocket
+import time
+
+
+def on_message(ws, message):
+	message_to_server = "pong"
+	print message
+	ws.send(message_to_server)
+	print (message_to_server+"\n")
+
+
+
+
+
+	
+
+def on_error(ws, error):
+	print error
+
+def on_close(ws):
+	print "### closed ###"
+
+def on_open(ws):
+	print "### open ###"
+
+
+
+
+if __name__ == "__main__":
+	websocket.enableTrace(False)
+	ws = websocket.WebSocketApp("ws://192.168.1.15:3000/",
+	on_message = on_message,
+	on_error = on_error,
+	on_close = on_close)
+	ws.on_open = on_open
+	ws.run_forever()
